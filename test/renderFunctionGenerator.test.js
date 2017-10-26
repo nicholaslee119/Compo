dfad9f2a79fb3548d1a3cb9a11e2abb4b04edbd0
Nicholas Lee <nicholaslee119@gmail.com>
2017-10-26 17:11:02 +0900

    implement {if}{/if}

--- test/renderFunctionGenerator.test.js
+++ test/renderFunctionGenerator.test.js
@@ -11,4 +11,9 @@ describe('renderFunctionGenerator tests', function(){
     const html = render(`<p>hello  {$title} {$name}!</p>`, {name:'nic', title:'Mr.'})
     expect(html).toBe('<p>hello  Mr. nic!</p>');
   })
+
+  it('test {if}{/if}', function(){
+    const html = render(`<p>hello  {if $flag}{$title} {/if}{$name}!</p>`, {name:'nic', title:'Mr.', flag:false})
+    expect(html).toBe('<p>hello  nic!</p>');
+  })
 });
